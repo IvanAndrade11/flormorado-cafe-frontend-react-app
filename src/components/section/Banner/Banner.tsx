ba22eda6ae3fd6545c2a252a9898d4462ada7be2
Ivan Vicente Andrade Ortiz <105233464+iandrade_adlover@users.noreply.github.com>
2026-03-02 18:45:19 -0500

    :bug: FMC-0003: fix video banner

--- src/components/section/Banner/Banner.tsx
+++ src/components/section/Banner/Banner.tsx
@@ -9,16 +9,18 @@ import { videos } from "@/utils/constants/videos";
 export const Banner: React.FC<IBanner> = ({ img }) => {
   return (
     <>
-      <div className="banner-container">
-        {isMobile ? (
-          <video className="banner-video" controls autoPlay muted loop>
+      {isMobile ? (
+        <div className="banner-video-container">
+          <video className="banner-video" autoPlay muted loop>
             <source src={videos.Bird} type="video/mp4" />
             Tu navegador no soporta el elemento video.
           </video>
-        ) : (
+        </div>
+      ) : (
+        <div className="banner-container">
           <img src={img} alt="Banner Flormorado Café" />
-        )}
-      </div>
+        </div>
+      )}
     </>
   );
 };
