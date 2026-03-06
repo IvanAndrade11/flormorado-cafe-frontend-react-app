488f55880219f04028a15c74f3be7a4e67b673ba
Ivan Vicente Andrade Ortiz <105233464+iandrade_adlover@users.noreply.github.com>
2026-03-05 23:37:33 -0500

    :sparkles: FMC-0005: Constants order and store configuration

---
+++ src/components/layout/NavbarStore/NavbarStore.tsx
@@ -0,0 +1,118 @@
+import { INavbarStore } from "@/types/components";
+import "./NavbarStore.scss";
+
+import { CAFE_FILTERS, icons, STORE_ORDER_BY } from "@/utils/constants";
+
+import { useState } from "react";
+
+import {
+  Button,
+  Container,
+  Nav,
+  Offcanvas,
+  Navbar,
+  NavDropdown,
+  Accordion,
+  Form,
+} from "react-bootstrap";
+
+export const NavbarStore: React.FC<INavbarStore> = ({ orderBy, filter }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
+
+  return (
+    <Navbar expand="false">
+      <Container fluid>
+        <Navbar.Brand>
+          <Button
+            variant="link"
+            onClick={() => toggleShow()}
+            className="fmc-navbar-store-show"
+          >
+            <>
+              <img
+                src={icons.FilterPurple}
+                alt="Tienda - Flormorado Café"
+                width="25"
+                className="d-inline-block align-top me-2"
+              />
+              <span className="fmc-store-filter-text">Filtros</span>
+            </>
+          </Button>
+        </Navbar.Brand>
+
+        <Navbar.Offcanvas
+          id={`navbar-filter-store`}
+          aria-labelledby={`navbar-label-filter-store`}
+          placement="start"
+          show={show}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`navbar-label-filter-store`}>
+              <h1 className="fmc-offcanvas-title">Filtros</h1>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              {CAFE_FILTERS.map((item) => (
+                <Accordion defaultActiveKey="0" className="my-2">
+                  <Accordion.Item eventKey={item.id} key={item.id}>
+                    <Accordion.Header>{item.value}</Accordion.Header>
+
+                    {item.options.map((option) => (
+                      <Accordion.Body
+                        key={option.id}
+                        className="fmc-store-offcanvas-accordion"
+                      >
+                        <Form.Check
+                          className="me-2"
+                          aria-label="option 1"
+                          onChange={(e) => {
+                            console.log(
+                              `Checkbox marcado: ${option.value}`,
+                              e.target.checked,
+                            );
+                          }}
+                        />
+                        <span>{option.value}</span>
+                      </Accordion.Body>
+                    ))}
+                  </Accordion.Item>
+                </Accordion>
+              ))}
+            </Nav>
+          </Offcanvas.Body>
+
+          <Button
+            onClick={() => handleClose()}
+            className="fmc-button mb-5 mx-3"
+          >
+            Aplicar Filtros
+          </Button>
+        </Navbar.Offcanvas>
+
+        <NavDropdown
+          className="fmc-store-filter-text"
+          title="Ordenar por"
+          id="nav-dropdown"
+          drop="down"
+          align="end"
+        >
+          {STORE_ORDER_BY.map((item) => (
+            <NavDropdown.Item
+              key={item.id}
+              className="fmc-store-dropdown-item"
+              onClick={() => orderBy(item.id)}
+            >
+              {item.value}
+            </NavDropdown.Item>
+          ))}
+        </NavDropdown>
+      </Container>
+    </Navbar>
+  );
+};
---
+++ src/utils/constants/store/index.ts
@@ -0,0 +1,85 @@
+export const STORE_ORDER_BY = [
+  {
+    id: "priceMayus",
+    value: "Mayor precio",
+  },
+  {
+    id: "priceMinus",
+    value: "Menor precio",
+  },
+  {
+    id: "nameDescendant",
+    value: "Nombre descendente",
+  },
+  {
+    id: "nameAscendant",
+    value: "Nombre ascendente",
+  },
+  {
+    id: "older",
+    value: "Más antiguos",
+  },
+  {
+    id: "newer",
+    value: "Más recientes",
+  },
+];
+
+export const CAFE_FILTERS = [
+  {
+    id: "brand",
+    value: "Marca",
+    options: [
+      {
+        id: 0,
+        value: "Flormorado",
+      },
+      {
+        id: 1,
+        value: "Oca",
+      },
+    ],
+  },
+  {
+    id: "size",
+    value: "Tamaño",
+    options: [
+      {
+        id: 0,
+        value: "340g",
+      },
+      {
+        id: 1,
+        value: "500g",
+      },
+    ],
+  },
+  {
+    id: "origin",
+    value: "Origen",
+    options: [
+      {
+        id: 0,
+        value: "Somondoco",
+      },
+    ],
+  },
+  {
+    id: "profile",
+    value: "Perfil de taza",
+    options: [
+      {
+        id: 0,
+        value: "Balanceado",
+      },
+      {
+        id: 1,
+        value: "Fuerte",
+      },
+      {
+        id: 2,
+        value: "Suave",
+      },
+    ],
+  },
+];
